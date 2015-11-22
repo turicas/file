14bedd7d51c30764e4eacfc572869fd73b0ea655
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-11-22 06:31:56 -0200

    Update setup.py with new version

--- python/setup.py
+++ python/setup.py
@@ -1,16 +1,19 @@
 # coding: utf-8
 
+from __future__ import unicode_literals
+
 from setuptools import setup
 
 
 setup(name='file-magic',
-      version='0.2.0',
-      author='Reuben Thomas',
-      author_email='rrt@sc3d.org',
+      version='0.3.0',
+      author='Reuben Thomas, Álvaro Justen',
+      author_email='rrt@sc3d.org, alvarojusten@gmail.com',
       url='https://github.com/file/file',
       license='BSD',
       description='(official) libmagic Python bindings',
       py_modules=['magic'],
+      test_suite='tests',
       classifiers = [
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
